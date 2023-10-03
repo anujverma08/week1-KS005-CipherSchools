27b6b50729f8288ac729944161ca722c00d42388
Anuj <anujvermagkp@gmail.com>
2023-10-03 11:56:07 +0530

    new file:   .vscode/settings.json 	new file:   lecture5part1.cpp

---
+++ lecture5part1.cpp
@@ -0,0 +1,12 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    int a[5];
+    int a,b,c,d,e;
+    cout << "give 5 integers as input" << endl;
+    cin >> a >> b >> c >> d >> e;
+    cout << "reverse order is : " << e << d << c << b << a;
+    }
+    
